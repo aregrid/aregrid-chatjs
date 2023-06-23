a039df39f62c010b33cfdc13224568afa2f8a5bf
tzhangchi <c@affine.pro>
2023-06-23 11:32:06 +0800

    feat: add tailwind and code copilot

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
 <script setup>
 import CCDraw from "./components/CCDraw.vue";
 </script>
-
 <template>
-  <div class="banner">CCDraw</div>
-  <div class="ccdraw-wrapper">
-    <CCDraw />
+  <div class="h-screen bg-gray-100 flex">
+    <aside class="w-1/4 bg-gray-200 p-4">
+      <h2 class="text-lg font-semibold text-gray-800 mb-4">Sidebar</h2>
+      <!-- Sidebar content goes here -->
+    </aside>
+
+    <div class="flex-1 bg-white">
+      <header class="bg-white border-b border-gray-300 p-4">
+        <h1 class="text-xl font-semibold text-gray-800">
+          Welcome to our Whiteboard
+        </h1>
+      </header>
+
+      <main class="p-4">
+        <h2 class="text-lg font-semibold text-gray-800 mb-4">Whiteboard</h2>
+        <div class="bg-white rounded-lg shadow-md p-6">
+          <!-- Whiteboard content goes here -->
+          <CCDraw />
+        </div>
+      </main>
+
+      <footer class="bg-white border-t border-gray-300 p-4 text-gray-600">
+        <p>Footer content goes here</p>
+      </footer>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.banner {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 3em;
-  background-color: #646cff;
-  color: white;
-  font-size: 1.5em;
-  font-weight: bold;
-}
-.ccdraw-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-  box-shadow: 0 0 1em #00000022;
-}
-</style>
+<style scoped></style>
