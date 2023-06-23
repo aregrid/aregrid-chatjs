4edbd1a55f39ecdcbaccffe443e3701ef373e25e
tzhangchi <c@affine.pro>
2023-06-23 22:06:29 +0800

    chore: house kepping

--- apps/aregridjs/src/app/message-blocksuite/message-blocksuite.component.ts
+++ apps/aregridjs/src/app/message-blocksuite/message-blocksuite.component.ts
@@ -13,23 +13,13 @@ import { SimpleAffineEditor } from '@blocksuite/editor';
   selector: 'aregrid-message-blocksuite',
   standalone: true,
   imports: [CommonModule],
-  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './message-blocksuite.component.html',
   styleUrls: ['./message-blocksuite.component.scss'],
-  // encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class MessageBlocksuiteComponent implements AfterViewInit {
   @ViewChild('messageBox') messageBox: ElementRef<HTMLDivElement> | null = null;
   constructor(private renderer: Renderer2) {}
   ngAfterViewInit() {
-    // ElementRef: { nativeElement: <input> }
-    // console.log(this.input);
-
-    // // Access the input object or DOM node
-    // console.dir(this.input.nativeElement);
-
-    // Manipulate via Renderer2
-    // this.renderer.setStyle(this.input.nativeElement, 'background', '#d515a0');
     const editor = new SimpleAffineEditor();
     this.messageBox && this.messageBox.nativeElement.appendChild(editor);
   }
