128b85269bd3f763d74e22d97242c22d1dcefc55
tzhangchi <c@affine.pro>
2023-06-23 17:55:34 +0800

    feat: add aregrid-message-blocksuite

--- apps/aregridjs/src/app/chat/chat.component.ts
+++ apps/aregridjs/src/app/chat/chat.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MessageBlocksuiteComponent } from '../message-blocksuite/message-blocksuite.component';
 
 @Component({
   selector: 'aregrid-chat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MessageBlocksuiteComponent],
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
---
+++ apps/aregridjs/src/app/message-blocksuite/message-blocksuite.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'aregrid-message-blocksuite',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './message-blocksuite.component.html',
+  styleUrls: ['./message-blocksuite.component.scss'],
+})
+export class MessageBlocksuiteComponent {}
