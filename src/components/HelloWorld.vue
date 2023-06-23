df672cc6a77f61d0da748cdd84249a78ad16e61c
tzhangchi <c@affine.pro>
2023-06-23 10:26:22 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+import { SimpleAffineEditor } from "@blocksuite/editor";
+import "@blocksuite/editor/themes/affine.css";
+
+const editor = new SimpleAffineEditor();
+document.body.appendChild(editor);
+</script>
+
+<template></template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
