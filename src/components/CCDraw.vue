6556892b848d6a35afcf529a8ebdc85fd67a02dc
tzhangchi <c@affine.pro>
2023-06-23 11:03:05 +0800

    feat: rename host

--- src/components/CCDraw.vue
+++ src/components/CCDraw.vue
@@ -1,11 +1,10 @@
 <script setup>
-import { ref } from "vue";
 import { SimpleAffineEditor } from "@blocksuite/editor";
 import "@blocksuite/editor/themes/affine.css";
 
 try {
-  const editor = new SimpleAffineEditor();
-  document.body.appendChild(editor);
+  const host = new SimpleAffineEditor();
+  document.body.appendChild(host);
   // editor.mode = "edgeless";
   window.editor = document.querySelector("editor-container");
   // window.editor.mode = "edgeless";
