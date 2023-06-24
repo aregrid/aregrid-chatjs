5632f90db0c3522ad8623d7220d05de0218cb468
tzhangchi <c@affine.pro>
2023-06-24 17:04:26 +0800

    feat: add new header

--- apps/aregridjs/src/app/chat/chat.component.ts
+++ apps/aregridjs/src/app/chat/chat.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageBlocksuiteComponent } from '../message-blocksuite/message-blocksuite.component';
-
+import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'aregrid-chat',
   standalone: true,
-  imports: [CommonModule, MessageBlocksuiteComponent],
+  imports: [CommonModule, MessageBlocksuiteComponent, HeaderComponent],
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
