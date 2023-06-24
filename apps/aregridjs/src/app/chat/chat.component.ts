f3d4a0d980a12b70018bd9d9397c3e48f4e1db85
tzhangchi <c@affine.pro>
2023-06-24 17:18:04 +0800

    feat: make chatMessages  driven chatMessages render

--- apps/aregridjs/src/app/chat/chat.component.ts
+++ apps/aregridjs/src/app/chat/chat.component.ts
@@ -15,4 +15,20 @@ import { ChatMessageComponent } from './chat-message/chat-message.component';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent {}
+export class ChatComponent {
+  chatMessages = [
+    {
+      avatar:
+        'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/005099b73f79ca48a6acf026c0021f93f323e9ba_full.jpg',
+      userName: 'John Doe',
+      content: 'Hello, how are you?',
+    },
+    {
+      avatar:
+        'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/00/0077f8c11bd1f32199710a0259c9873ce8e7b63b_full.jpg',
+      userName: 'Jane Smith',
+      content: '👋',
+    },
+    // Add more chat messages here...
+  ];
+}
