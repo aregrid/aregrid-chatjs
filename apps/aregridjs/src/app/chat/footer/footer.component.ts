c223b40d467b872e46c914f07da426515d33534a
tzhangchi <c@affine.pro>
2023-06-26 17:56:38 +0800

    feat: affine represent a doc

--- apps/aregridjs/src/app/chat/footer/footer.component.ts
+++ apps/aregridjs/src/app/chat/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MessageBlocksuiteComponent } from '../../message-blocksuite/message-blocksuite.component';
 
 @Component({
   selector: 'aregrid-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MessageBlocksuiteComponent],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
--- apps/aregridjs/src/app/message-blocksuite/message-blocksuite.component.ts
+++ apps/aregridjs/src/app/message-blocksuite/message-blocksuite.component.ts
@@ -4,6 +4,7 @@ import {
   ElementRef,
   AfterViewInit,
   Renderer2,
+  Input,
 } from '@angular/core';
 import pageSnapShot from './page-snapshot-mock';
 import { CommonModule } from '@angular/common';
@@ -19,15 +20,19 @@ import { SimpleAffineEditor } from '@blocksuite/editor';
 export class MessageBlocksuiteComponent implements AfterViewInit {
   @ViewChild('messageBox') messageBox: ElementRef<HTMLDivElement> | null = null;
   constructor(private renderer: Renderer2) {}
+  @Input() templateId: string | undefined;
+
   ngAfterViewInit() {
     const editor = new SimpleAffineEditor();
     this.messageBox && this.messageBox.nativeElement.appendChild(editor);
 
     (window as any).blocksuiteEditor = editor;
 
-    editor.workspace.importPageSnapshot(
-      pageSnapShot.data['space:hello-world'],
-      'page0'
-    );
+    if (this.templateId === 'affine') {
+      editor.workspace.importPageSnapshot(
+        pageSnapShot.data['space:hello-world'],
+        'page0'
+      );
+    }
   }
 }
