23bebef90076f0c8ee1e61633a05a8dc8e322099
tzhangchi <c@affine.pro>
2023-07-10 16:28:44 +0800

    feat: add note ai page

--- apps/aregridjs/src/app/app.routes.ts
+++ apps/aregridjs/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Route } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
-
+import { NoteAiComponent } from './note-ai/note-ai.component';
 export const appRoutes: Route[] = [
   {
     path: '',
     component: ChatComponent,
     title: 'Channels | Aregrid',
+  },
+  {
+    path: 'note-ai',
+    component: NoteAiComponent,
+    title: 'Note AI | Aregrid',
   },
 ];
---
+++ apps/aregridjs/src/app/note-ai/note-ai.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'aregrid-note-ai',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './note-ai.component.html',
+  styleUrls: ['./note-ai.component.scss'],
+})
+export class NoteAiComponent {}
