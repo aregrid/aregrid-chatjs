512054a338147e0bfeeec0fa78e4cc8306aedfb0
tzhangchi <c@affine.pro>
2023-07-12 17:58:40 +0800

    feat: add env

--- apps/note-ai/src/main.ts
+++ apps/note-ai/src/main.ts
@@ -5,3 +5,6 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
+
+console.log(`NODE_ENV`, process.env['NODE_ENV']);
+console.log(`NX_API_URL`, process.env['NX_API_URL']);
